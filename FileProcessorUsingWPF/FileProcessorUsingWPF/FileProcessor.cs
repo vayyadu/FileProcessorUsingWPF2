3fb6a973c1b19b32ff793e504be78ea6eff31271
vayyadu <vijay.ayyadurai@gmail.com>
2014-06-11 23:47:44 -0600

    File Processor Using WPF
    File Processor Using WPF

---
+++ FileProcessorUsingWPF/FileProcessorUsingWPF/FileProcessor.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// addded this namespace to make use of Task class to achieve multithreading needs of this task
+using System.Threading.Tasks;
+//added this namespace to make use of DirectoryInfo class to access and collect information about the files and folders in the directories
+using System.IO;
+// added this namespace to format the results
+using System.Globalization;
+
+namespace FileProcessorUsingWPF
+{
+    class FileProcessor
+    {
+        // delegate that is used as a pointer for the method to update the result and close the progressbar window
+        public delegate void OnWorkerMethodCompleteDelegate(string result);
+        // event that is used to indicate the tasks running on the threads are complete so that the results could be updated on the main window
+        public event OnWorkerMethodCompleteDelegate OnWorkerComplete;
+
+
+        public void WorkerMethod(string[] filePath, bool includeSubDir)
+        {
+            try
+            {
+                long result;
+
+                
+                // create 3 different threads to calculate the directory size (one thread for each one the 3 user input directories) 
+                var task1 = Task.Factory.StartNew(() => result = GetAggregateDirectorySize(filePath[0], true));
+                var task2 = Task.Factory.StartNew(() => result = GetAggregateDirectorySize(filePath[1], true));
+                var task3 = Task.Factory.StartNew(() => result = GetAggregateDirectorySize(filePath[2], true));
+
+                // this will ensure the threads wait until all three tasks are completed
+                Task.WaitAll();
+
+                // add the results from all the 3 threads to get the aggregate of file sizes from all 3 user input directories
+                result = task1.Result + task2.Result + task3.Result;
+
+                // raise the event to indicate that the tasks on all 3 threads are complete so that the UI will be updated accordingly
+                OnWorkerComplete("The aggregate of all the file sizes is "
+                                    + String.Format(CultureInfo.InvariantCulture, "{0:#,#}", result) + " bytes ("
+                                    + String.Format(CultureInfo.InvariantCulture, "{0:#,#}", (result / 1024)) + " KB / "
+                                    + String.Format(CultureInfo.InvariantCulture, "{0:#,#}", (result / (1024 * 1024))) + " MB" + ")");
+            }
+            catch (Exception ex)
+            {
+                OnWorkerComplete("Error has occurred. Please try again. Additional error information: " + ex.Message);
+            }
+        }
+
+
+
+        // method will calculate the directory size and it calls the same method recursively to include all the subdirectories
+        public long GetAggregateDirectorySize(string filePath, bool includeSubDir)
+        {
+            try
+            {
+                DirectoryInfo dInfo = new DirectoryInfo(filePath);
+
+                // Enumerate all the files
+                long aggregateFileSize = dInfo.EnumerateFiles().Sum(file => file.Length);
+
+                // If Subdirectories are to be included
+                if (includeSubDir)
+                {
+                    // Enumerate all sub-directories and call the same method recursively
+                    aggregateFileSize += dInfo.EnumerateDirectories().Sum(dir => GetAggregateDirectorySize(dir.FullName, true));
+                }
+                return aggregateFileSize;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+      
+    }
+}
+
---
+++ FileProcessorUsingWPF/FileProcessorUsingWPF/MainWindow.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+// added this namespace to make use of class FolderBrowserDialog for browse funtionality
+using System.Windows.Forms;
+// added this namespace to make use of ThreadStart and Thread classes to spin a thread to do the task
+using System.Threading;
+
+namespace FileProcessorUsingWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ProgressBarWindow pbw = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Process_Click(object sender, RoutedEventArgs e)
+        {
+            OnWorkerMethodStart();
+        }
+
+        private void OnWorkerMethodStart()
+        {
+            try
+            {
+                // check if the user has selected all the 3 directories. If not display the error message and do nothing
+                if (textBox1.Text.Trim() == string.Empty || textBox2.Text.Trim() == string.Empty || textBox3.Text.Trim() == string.Empty)
+                {
+                    textBlock1.Text = "Please choose valid directories.";
+                    return;
+                }
+
+
+                
+                // disable the "Process" button as soon as the processing starts 
+                Process.IsEnabled = false;
+                textBlock1.Text = "Processing.. Please wait..";
+
+                //Create a string array to pass the user input directories to the worker method of the FileProcessor
+                string[] filePath = new string[3];
+                filePath[0] = textBox1.Text;
+                filePath[1] = textBox2.Text;
+                filePath[2] = textBox3.Text;
+
+                FileProcessor fileProcessor = new FileProcessor();
+
+                // wire the event to the corresponding handler
+                // OnWorkerComplete event will be raise after the 3 tasks are completed in 3 different tasks
+                // on raising this event, OnWorkerMethodComplete on the main window will be called to close the progressbar window and update the results to the user.
+                fileProcessor.OnWorkerComplete += new FileProcessor.OnWorkerMethodCompleteDelegate(OnWorkerMethodComplete);
+
+                // a separate thread is created and started to execute the WorkerMethod
+                ThreadStart tStart = new ThreadStart(() => fileProcessor.WorkerMethod(filePath, true));
+                Thread t = new Thread(tStart);
+                t.Start();
+
+                // display the progress bar to show the user that the program is still processing the task 
+                pbw = new ProgressBarWindow();
+                pbw.Owner = this;
+                pbw.ShowDialog();
+
+                // 
+                Process.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                textBlock1.Text = "Error has occurred. Please try again. Additional error information: " + ex.Message;
+            }
+        }
+
+        private void OnWorkerMethodComplete(string result)
+        {
+            // close the progressbar window after the task is completed
+            // use dispatcher invoke to close the UI element because the UI elements owned by UI(Main thread) cannot be access by the other thread
+            pbw.Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Normal,
+                                    new Action(
+                                                delegate()
+                                                {
+                                                    pbw.Close();
+                                                }
+                                    ));
+
+            // use dispatcher invoke to update the results to the user
+            textBlock1.Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Normal,
+                                       new Action(
+                                                   delegate()
+                                                   {
+                                                       textBlock1.Text = result;
+                                                   }
+                                       ));
+
+            
+        }
+
+
+        private void BrowseButton1_Click(object sender, RoutedEventArgs e)
+        {
+            //use FolderBrowseDialog class to set the user input directories to the textbox
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string path = dialog.SelectedPath;
+
+                textBox1.Text = path;
+            }
+        }
+
+        private void BrowseButton2_Click(object sender, RoutedEventArgs e)
+        {
+            //use FolderBrowseDialog class to set the user input directories to the textbox
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string path = dialog.SelectedPath;
+
+                textBox2.Text = path;
+            }
+        }
+
+        private void BrowseButton3_Click(object sender, RoutedEventArgs e)
+        {
+            //use FolderBrowseDialog class to set the user input directories to the textbox
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string path = dialog.SelectedPath;
+
+                textBox3.Text = path;
+            }
+        }
+    }
+}
